e535d56388569874d0b0e3e29f4755264c260dd1
Keito <keito@virusav.com>
2025-06-19 23:28:31 +0100

    Removed less then optimal camera renderer feedback, replaced with deck feedback

--- js/renderers/camera/renderer.js
+++ js/renderers/camera/renderer.js
@@ -33,35 +33,7 @@ window.hydra.renderers['camera'] = {
                 }
             }
         };
-        const ui = {
-            fieldsets: [
-                {
-                    heading: 'Feedback',
-                    class: 'flex',
-                    attributes: 'data-columns="2"',
-                    items: [
-                        {
-                            type: 'checkbox',
-                            label: 'Feedback Enabled',
-                            variable: 'feedbackEnabled',
-                            checked: true,
-                            randomiseable: true
-                        },
-                        {
-                            type: 'range',
-                            label: 'Feedback Level',
-                            variable: 'feedbackLevel',
-                            min: 0,
-                            max: 1,
-                            value: 0.005,
-                            step: 0.001,
-                            containerClass: 'grow',
-                            randomiseable: true
-                        },
-                    ]
-                }
-            ]
-        };
+        const ui = {};
         deck.camera = window.hydra.renderer.init(deck, 'camera', {defaults, ui});
 
         const constraints = {
@@ -71,8 +43,6 @@ window.hydra.renderers['camera'] = {
 
         let cameraShutoffInterval;
         let cameraStream;
-
-        deck.camera.image = new Image();
 
         let scale;
 
@@ -96,19 +66,6 @@ window.hydra.renderers['camera'] = {
 
             const ratio = deck.canvas.width / deck.streamEl.videoWidth;
             deck.ctx.drawImage(deck.streamEl, 0, 0, (deck.streamEl.videoWidth * ratio) * scale, (deck.streamEl.videoHeight * ratio) * scale);
-
-            if (deck.camera.feedbackEnabled) {
-
-                if (deck.camera.image !== undefined) {
-                    deck.ctx.save();
-                    deck.ctx.globalAlpha = deck.camera.feedbackLevel;
-                    deck.ctx.drawImage(deck.camera.image, 0, 0);
-                    deck.ctx.restore();
-                }
-
-                deck.camera.image.src = deck.canvas.toDataURL();
-            }
-
 
             cameraShutoffInterval = setTimeout(() => {
                 deck.camera.connected = false;
